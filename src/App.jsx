5c4b2e8b1a4b9826cc63e65c6c0e4220162862e0
FuadTalukder85 <webfuadtk@gmail.com>
2024-12-13 01:05:12 +0600

    add modal and show carts data

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,78 @@
 import "./App.css";
-import img01 from "../src/assets/img01.png";
+import purple from "../src/assets/purple.png";
 import black from "../src/assets/black.png";
 import blue from "../src/assets/blue.png";
 import cyan from "../src/assets/cyan.png";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import { useState } from "react";
+
 function App() {
+  const [selectedColor, setSelectedColor] = useState("#6576FF");
+  const [activeSize, setActiveSize] = useState("S");
+  const [quantity, setQuantity] = useState(1);
+  const [cart, setCart] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
   const colorToImagesMap = {
-    "#6576FF": [{ original: img01 }, { original: black }],
+    "#6576FF": [{ original: purple }, { original: black }],
     "#1FCEC9": [{ original: cyan }, { original: black }],
     "#4B97D3": [{ original: blue }, { original: blue }],
     "#3B4747": [{ original: black }, { original: cyan }],
   };
-  const [selectedColor, setSelectedColor] = useState("#6576FF");
 
-  const handleColorClick = (color) => {
-    setSelectedColor(color);
+  const colorNamesMap = {
+    "#6576FF": "purple",
+    "#1FCEC9": "cyan",
+    "#4B97D3": "blue",
+    "#3B4747": "black",
   };
 
+  const sizes = [
+    { label: "S", price: 69 },
+    { label: "M", price: 79 },
+    { label: "L", price: 89 },
+    { label: "XL", price: 99 },
+  ];
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+
+  // handle add to cart
+  const handleAddToCart = () => {
+    const selectedSize = sizes.find((size) => size.label === activeSize);
+
+    const selectedItem = {
+      title: "Classy Modern Smart Watch",
+      color: colorNamesMap[selectedColor],
+      size: activeSize,
+      quantity,
+      price: selectedSize.price,
+      image: colorToImagesMap[selectedColor][0].original,
+    };
+
+    setCart([...cart, selectedItem]);
+    console.log("Cart:", [...cart, selectedItem]);
+  };
+  //   handle show modal
+  const handleShowModal = () => {
+    setShowModal(!showModal);
+  };
+  const totalQuantity = cart?.reduce((sum, item) => sum + item.quantity, 0);
+  const totalPrice = cart?.reduce(
+    (sum, item) => sum + item?.price * item.quantity,
+    0
+  );
+
   return (
-    <div className="w-[1320px] mx-auto pt-28">
+    <div className="w-[1320px] mx-auto py-28">
       <div className="grid grid-cols-2 gap-14 items-center">
         <div className="col-span-1 rounded-lg">
           <ImageGallery
@@ -30,20 +82,20 @@ function App() {
             thumbnailPosition="bottom"
             showNav={false}
           />
-          {/* <img className="w-full rounded-md" src={img01} alt="" /> */}
         </div>
         <div className="col-span-1">
           <h1 className="text-4xl font-bold text-[#364A63]">
-            Classy Modern Smart watch
+            Classy Modern Smart Watch
           </h1>
           <p className="pt-3">
             ★★★★★ <span className="text-[#8091A7] text-sm">(2 Reviews)</span>
           </p>
-          {/* price */}
           <div className="pt-5">
             <span className="text-[#8091A7] line-through text-xl">$99.00</span>
             <span className="text-[#6576FF] font-bold text-2xl ps-2">
-              $79.00
+              $
+              {sizes.find((size) => size.label === activeSize).price * quantity}
+              .00
             </span>
           </div>
           <p className="pt-5 text-[#8091A7] text-lg">
@@ -51,28 +103,14 @@ function App() {
             praising pain was born and I will give you a complete account of the
             system, and expound the actual teaching.
           </p>
-          {/*  */}
-          <div className="pt-5 flex gap-10">
-            <div>
-              <p className="text-[#8091A7] text-sm">Type</p>
-              <p className="text-[#364A63] text-lg font-bold">Watch</p>
-            </div>
-            <div>
-              <p className="text-[#8091A7] text-sm">Model Number</p>
-              <p className="text-[#364A63] text-lg font-bold">
-                Forerunner 290XT
-              </p>
-            </div>
-          </div>
-          {/* color */}
           <div className="pt-5">
             <h3 className="text-[#364A63] text-lg font-bold">Brand Color</h3>
             <ul className="pt-1 flex gap-5">
               {Object.keys(colorToImagesMap).map((color) => (
                 <li
                   key={color}
-                  className={`w-5 h-5 rounded-full cursor-pointer flex items-center justify-center`}
-                  onClick={() => handleColorClick(color)}
+                  className="w-5 h-5 rounded-full cursor-pointer flex items-center justify-center"
+                  onClick={() => setSelectedColor(color)}
                   style={{
                     boxShadow:
                       selectedColor === color ? `0 0 0 2px ${color}` : "none",
@@ -88,60 +126,58 @@ function App() {
               ))}
             </ul>
           </div>
-          {/* size */}
           <div className="pt-5">
             <h3 className="text-[#364A63] text-lg font-bold">Wrist Size</h3>
             <ul className="pt-1 flex gap-4 text-sm">
-              <li className="group">
-                <button className="text-[#8091A7] border border-[#DBDFEA] px-5 py-1 rounded-[3px] group-hover:border-[#6576FF] transition-all duration-300">
-                  <span className="pr-2 text-[#364A63] font-bold group-hover:text-[#6576FF] transition-all duration-300">
-                    S
-                  </span>
-                  <span className="text-xs">$69</span>
-                </button>
-              </li>
-              <li className="group">
-                <button className="text-[#8091A7] border border-[#DBDFEA] px-5 py-1 rounded-[3px] group-hover:border-[#6576FF] transition-all duration-300">
-                  <span className="pr-2 text-[#364A63] font-bold group-hover:text-[#6576FF] transition-all duration-300">
-                    M
-                  </span>
-                  <span className="text-xs">$79</span>
-                </button>
-              </li>
-              <li className="group">
-                <button className="text-[#8091A7] border border-[#DBDFEA] px-5 py-1 rounded-[3px] group-hover:border-[#6576FF] transition-all duration-300">
-                  <span className="pr-2 text-[#364A63] font-bold group-hover:text-[#6576FF] transition-all duration-300">
-                    L
-                  </span>
-                  <span className="text-xs">$89</span>
-                </button>
-              </li>
-              <li className="group">
-                <button className="text-[#8091A7] border border-[#DBDFEA] px-5 py-1 rounded-[3px] group-hover:border-[#6576FF] transition-all duration-300">
-                  <span className="pr-2 text-[#364A63] font-bold group-hover:text-[#6576FF] transition-all duration-300">
-                    XL
-                  </span>
-                  <span className="text-xs">$99</span>
-                </button>
-              </li>
+              {sizes.map((size) => (
+                <li key={size.label} className="group">
+                  <button
+                    onClick={() => setActiveSize(size.label)}
+                    className={`text-[#8091A7] border px-5 py-1 rounded-[3px] transition-all duration-300 ${
+                      activeSize === size.label
+                        ? "border-[#6576FF]"
+                        : "border-[#DBDFEA]"
+                    }`}
+                  >
+                    <span
+                      className={`pr-2 font-bold transition-all duration-300 ${
+                        activeSize === size.label
+                          ? "text-[#6576FF]"
+                          : "text-[#364A63]"
+                      }`}
+                    >
+                      {size.label}
+                    </span>
+                    <span className="text-xs">${size.price}</span>
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
-          {/* cart */}
           <div className="mt-5">
-            <ul className="flex gap-4 items-center">
+            <ul className="flex gap-4 items-center text-[#8091A7]">
               <li>
-                <button className="border-l border-y border-[#DBDFEA] px-3 py-1 rounded-l-[3px]">
+                <button
+                  onClick={handleDecrement}
+                  className="border-l border-y border-[#DBDFEA] px-3 py-1 rounded-l-[3px]"
+                >
                   -
                 </button>
-                <button className="text-[#DBDFEA] border border-[#DBDFEA] px-7 py-1">
-                  0
+                <button className="text-[#364A63] border border-[#DBDFEA] px-7 py-1">
+                  {quantity}
                 </button>
-                <button className="border-r border-y border-[#DBDFEA] px-3 py-1 rounded-r-[3px]">
+                <button
+                  onClick={handleIncrement}
+                  className="border-r border-y border-[#DBDFEA] px-3 py-1 rounded-r-[3px]"
+                >
                   +
                 </button>
               </li>
               <li>
-                <button className="bg-[#6576FF] text-white px-3 py-1 rounded-[3px]">
+                <button
+                  onClick={handleAddToCart}
+                  className="bg-[#6576FF] text-white px-3 py-1 rounded-[3px]"
+                >
                   Add to Cart
                 </button>
               </li>
@@ -150,6 +186,70 @@ function App() {
           </div>
         </div>
       </div>
+      <div className="flex justify-center mt-28">
+        <button
+          onClick={handleShowModal}
+          className="px-6 py-2 bg-[#FFBB5A] text-[#364A63] font-bold rounded-3xl"
+        >
+          Checkout{" "}
+          <span className="text-sm ml-2 bg-white py-1 px-2 rounded-lg">
+            {cart.length}
+          </span>
+        </button>
+      </div>
+      {showModal && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white rounded-lg w-[650px] px-14 py-16">
+            <h2 className="text-2xl font-bold mb-6">Your Cart</h2>
+            <table className="w-full text-left">
+              <thead>
+                <tr className="text-gray-600">
+                  <th className="py-3">Item</th>
+                  <th className="py-3">Color</th>
+                  <th className="py-3">Size</th>
+                  <th className="py-3">Qnt</th>
+                  <th className="py-3 text-right">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart?.map((item, index) => (
+                  <tr key={index} className="text-sm border-b">
+                    <td className="w-6/12 py-3">
+                      <img
+                        src={item.image}
+                        alt={item.title}
+                        className="inline-block w-12 h-12 rounded-md mr-4"
+                      />
+                      {item.title}
+                    </td>
+                    <td className="py-3 capitalize">{item.color}</td>
+                    <td className="py-3 font-bold">{item.size}</td>
+                    <td className="py-3">{item.quantity}</td>
+                    <td className="py-3 text-right">
+                      ${item.price * item.quantity}.00
+                    </td>
+                  </tr>
+                ))}
+                <tr className="bg-gray-100 font-bold">
+                  <td className="py-3">Total</td>
+                  <td></td>
+                  <td></td>
+                  <td className="py-3">{totalQuantity}</td>
+                  <td className="py-3">${totalPrice}.00</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="flex justify-between items-center mt-6">
+              <button className="px-4 py-2 bg-gray-200 text-gray-600 rounded-md hover:bg-gray-300">
+                Continue Shopping
+              </button>
+              <button className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
